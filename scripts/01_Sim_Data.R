7f2e642f96ef4063ddfd9668e45f69fb6e91a3cb
Nafa <adamnafa@my.unt.edu>
2022-01-28 21:34:31 -0600

    Update the simulation data to allow time invariant confounding for the outcome and treatment

--- scripts/01_Sim_Data.R
+++ scripts/01_Sim_Data.R
@@ -28,7 +28,7 @@ source("scripts/00_Helper_Functions.R")
 
 # Defining the function for simulating cross-sectional time series data----
 sim_AR1_xts <- function(J, T, resp_coefs = NULL, 
-                        treat_coefs = NULL, seed = 12345, ...) {
+                        treat_coefs = NULL, seed = 12345, conf, ...) {
   # Define the Dimensions for the Simulated Data
   Countries <- J # Number of Countries
   Periods <- T # Number of Time Periods
@@ -40,13 +40,13 @@ sim_AR1_xts <- function(J, T, resp_coefs = NULL,
   # Check if user-specified covariates for the response are provided----
   if (is.null(resp_coefs)) {
     # If no coefficient values are provided simulate from a normal dist
-    resp_coef_vals <- sample(rnorm(N, 0, 3), 4)
-    names(resp_coef_vals) <- c("treat", "beta", "gamma", "delta")
+    resp_coef_vals <- sample(rnorm(N, 0, 3), 5)
+    names(resp_coef_vals) <- c("intercept", "treat", "beta", "gamma", "delta")
   }
   # If user covariate values of length 5 are provided use those
-  else if (!is.null(resp_coefs) & length(resp_coefs) == 4) {
+  else if (!is.null(resp_coefs) & length(resp_coefs) == 5) {
     resp_coef_vals <- resp_coefs
-    names(resp_coef_vals) <- c("treat", "beta", "gamma", "delta")
+    names(resp_coef_vals) <- c("intercept", "treat", "beta", "gamma", "delta")
   }
   # Throw an error is length of provided vector is wrong
   else {
@@ -77,7 +77,7 @@ sim_AR1_xts <- function(J, T, resp_coefs = NULL,
     time = seq(1, Periods, 1)
   )
   
-  # For y[1[j]] ~ half-normal(mu[1[j]], 3)
+  # For y[1[j]] ~ half-normal(5, 10)
   resp_grid$Y[resp_grid$time == 1] <- 
     rtruncnorm(Countries, a = 0, mean = 5, sd = 10)
   
@@ -114,6 +114,11 @@ sim_AR1_xts <- function(J, T, resp_coefs = NULL,
     # Coerce identifiers back to numeric
     mutate(across(time:country, ~ as.numeric(.x)))
   
+  # Simulate country-specific time invariant confounding
+  for (j in 1:Countries) {
+    resp_grid$Conf[resp_grid$country == j] <- runif(1, -conf, conf)
+  }
+  
   # Simulate the data for the fixed effects parameters----
   fixeffs_grid <- expand_grid(
     country = seq(1, Countries, 1),
@@ -146,36 +151,49 @@ sim_AR1_xts <- function(J, T, resp_coefs = NULL,
         rnorm(1, fixeffs_grid[time == (t - 1) & country == j]$delta, sd = 1.25)
     }
   }
-  
+
   # Construct the full data
-  treatment_grid <- resp_grid %>% 
+  treatment_grid <- resp_grid %>%
     # Combine the two data frames
-    left_join(fixeffs_grid, by = c("time", "country")) %>% 
-    # Group the data by country
-    group_by(country) %>% 
+    left_join(fixeffs_grid, by = c("time", "country")) %>%
     # Create the treatment process
     mutate(
       # Model the treatment assignment as a function of time and the covariates
-      treat = treat_coef_vals["intercept"] + (time/1000) + 
-        (treat_coef_vals["beta"] * beta) + (treat_coef_vals["gamma"] * gamma) + 
-        (treat_coef_vals["delta"] * delta) + rnorm(n(), 0, 0.25),
-      # Generate a binary indicator after eight treatment status change
-      post_treat = ifelse(rleid(rbinom(n(), 1, prob = rescale(treat, to = c(0, 0.25)))) >= 8, 1, 0),
-      # Induce dependence of the response on the treatment
-      y_treated = Y + lag(Y, n = 1L) + (resp_coef_vals["beta"] * beta) + 
-        (resp_coef_vals["gamma"] * gamma) + (resp_coef_vals["delta"] * delta) + 
-        (resp_coef_vals["treat"] * post_treat) + rnorm(n(), 0, 0.5),
+      treat = treat_coef_vals["intercept"] + (0.0005*(time - 2)) +
+        (treat_coef_vals["beta"] * beta) + (treat_coef_vals["gamma"] * gamma) +
+        (treat_coef_vals["delta"] * delta) + rnorm(N, 0, 0.5),
+      # Generate confounded treatment
+      treat_conf = treat + Conf,
+      # Rescale the latent treatment
+      treat_scaled = rescale(treat, to = c(0, 1)),
+      # Rescale the confounded latent treatment
+      treat_scaled_conf = rescale(treat_conf, to = c(0, 1)),
+      # Generate a binary event indicator
+      treat_bin = rbinom(N, 1, plogis(treat_scaled)),
+      # Generate a confounded binary event indicator
+      treat_bin_conf = rbinom(N, 1, plogis(treat_scaled_conf)),
       # Recode Time
-      time = time - 2,
-      # Generate lags of the covariates and treatment
-      across(
-        Y:y_treated,
-        ~ lag(.x, 1L),
-        .names = "{.col}_lag"
-      )
-    ) %>% 
+      time = time - 1
+    ) %>%
+    # Group the data by country
+    group_by(country) %>%
+    # Induce dependence of the response on the treatment
+    mutate(
+      # Confounded Response and Unconfounded Treatment
+      y_conf_treated = resp_coef_vals["intercept"] + (resp_coef_vals["beta"] * beta) +
+        (resp_coef_vals["gamma"] * gamma) + (resp_coef_vals["delta"] * delta) +
+        (resp_coef_vals["treat"] * treat_bin) + rnorm(n(), 0, 1) + Conf,
+      # Unconfounded Response and Confounded Treatment
+      y_treated_conf = resp_coef_vals["intercept"] + (resp_coef_vals["beta"] * beta) +
+        (resp_coef_vals["gamma"] * gamma) + (resp_coef_vals["delta"] * delta) +
+        (resp_coef_vals["treat"] * treat_bin_conf) + rnorm(n(), 0, 1),
+      # Confounded Response and Unconfounded Treatment
+      y_conf_treated_conf = resp_coef_vals["intercept"] + (resp_coef_vals["beta"] * beta) +
+        (resp_coef_vals["gamma"] * gamma) + (resp_coef_vals["delta"] * delta) +
+        (resp_coef_vals["treat"] * treat_bin_conf) + rnorm(n(), 0, 1) + Conf
+    ) %>%
     # Ungroup the data
-    ungroup() %>% 
+    ungroup() %>%
     # Drop missing values induced by lags
     drop_na()
   
@@ -207,6 +225,9 @@ sim_AR1_xts <- function(J, T, resp_coefs = NULL,
 #                of length 4 of the form `c(intercept, beta, gamma, delta)`.
 #                See description below for more information.
 #
+#  conf          A numeric value for the magnitude of time invariant 
+#                confounding. Value is passed to `runif(-conf, conf)`
+#
 #  ...           Additional arguments for future development. Currently 
 #                unused
 
@@ -217,6 +238,7 @@ sim_AR1_xts <- function(J, T, resp_coefs = NULL,
 #   X_{4tj} = \alpha + \0.001 \cdot time_{tj} + Z_{1tj}\beta + 
 #       Z_{2tj}\gamma + Z_{3tj}\delta + \upsilon
 # where \upsilon \sim N(0, \sigma_{j})
+#
 # The baseline values for the response vector are simulated based on
 # an AR(1) correlation structure. The observed response is a function
 # of the stochastic evolution of the AR(1) process, the intervention,
@@ -245,7 +267,7 @@ sim_AR1_xts <- function(J, T, resp_coefs = NULL,
 set.seed(12345)
 
 # Generate a vector of true values for the response coefficients
-treat_trues <- rnorm(4, 0, 2)*rgamma(4, 1, 1)
+treat_trues <- rnorm(4, 0, 1)*rgamma(4, 1, 1)
 
 # Simulate the data with 30 countries and 102 years and user-specified
 # coefficient values for the response and treatment. I use 102 here
@@ -254,12 +276,16 @@ sim_data <- sim_AR1_xts(
   J = 30, 
   T = 102, 
   seed = 12345,
-  resp_coefs = c(6.8563, 3.1879, -2.4487, 5.4057),
-  treat_coefs = treat_trues
+  resp_coefs = c(12.3591, 6.8563, 3.1879, -2.4487, 5.4057),
+  treat_coefs = treat_trues,
+  conf = 15
   )
 
-# Check distribution of the treatment
-xtabs(~ country + post_treat, sim_data$data)
+# Check distribution of the unconfounded treatment
+xtabs(~ country + treat_bin, sim_data$data)
+
+# Check distribution of the confounded treatment
+xtabs(~ country + treat_bin_conf, sim_data$data)
 
 # Print the true values of the coefficients in the response
 sim_data$true_resp
@@ -267,25 +293,71 @@ sim_data$true_resp
 # Plot a subset of the simulated data
 sim_data$data %>% 
   # Extract a subset of the data
-  filter(country %in% 1:12) %>% 
+  filter(country %in% 1:9) %>% 
   # Drop missing values in time
   drop_na() %>% 
   # Initiate a ggplot object
-  ggplot(aes(x = time, y = y_treated, color = as_factor(post_treat))) +
+  ggplot(aes(x = time, y = y_conf_treated, color = as_factor(treat_bin))) +
   # Facet the plot by group
   facet_wrap(~ country, scales = "free_y") + 
   # Add points for each value of y
   geom_point(size = 1.25) +
-  # Add a line between each point
-  geom_line() +
   # Customize the colors
   scale_color_manual(values = c("firebrick", "royalblue")) +
   # Set plot labels
   labs(
     x = "Time",
     y = "Response",
-    title = "Simulated AR(1) Panel Data",
-    color = "Post-Intervention"
+    title = "Simulated AR(1) Panel Data with Confounded Response and Unconfounded Treatment",
+    color = "Treated"
+  ) +
+  # Set plot theme
+  plot_theme(plot.margin = margin(3, 5, 5, 5, "mm"))
+
+# Plot a subset of the simulated data with confounding
+sim_data$data %>% 
+  # Extract a subset of the data
+  filter(country %in% 1:9) %>% 
+  # Drop missing values in time
+  drop_na() %>% 
+  # Initiate a ggplot object
+  ggplot(aes(x = time, y = y_treated_conf, color = as_factor(treat_bin_conf))) +
+  # Facet the plot by group
+  facet_wrap(~ country, scales = "free_y") + 
+  # Add points for each value of y
+  geom_point(size = 1.25) +
+  # Customize the colors
+  scale_color_manual(values = c("firebrick", "royalblue")) +
+  # Set plot labels
+  labs(
+    x = "Time",
+    y = "Response",
+    title = "Simulated AR(1) Panel Data with Unconfounded Response and Confounded Treatment",
+    color = "Treated"
+  ) +
+  # Set plot theme
+  plot_theme(plot.margin = margin(3, 5, 5, 5, "mm"))
+
+# Plot a subset of the simulated data with confounding
+sim_data$data %>% 
+  # Extract a subset of the data
+  filter(country %in% 1:9) %>% 
+  # Drop missing values in time
+  drop_na() %>% 
+  # Initiate a ggplot object
+  ggplot(aes(x = time, y = y_conf_treated_conf, color = as_factor(treat_bin_conf))) +
+  # Facet the plot by group
+  facet_wrap(~ country, scales = "free_y") + 
+  # Add points for each value of y
+  geom_point(size = 1.25) +
+  # Customize the colors
+  scale_color_manual(values = c("firebrick", "royalblue")) +
+  # Set plot labels
+  labs(
+    x = "Time",
+    y = "Response",
+    title = "Simulated AR(1) Panel Data with Confounded Response and Confounded Treatment",
+    color = "Treated"
   ) +
   # Set plot theme
   plot_theme(plot.margin = margin(3, 5, 5, 5, "mm"))
